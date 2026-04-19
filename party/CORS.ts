13a41904bf984b22aee13aaef8bc4416ab7c0e32
Moldybot9411 <joewhite8844@gmail.com>
2026-04-19 18:17:46 +0200

    fix: forgot cors

--- party/CORS.ts
+++ party/CORS.ts
@@ -1,5 +1,5 @@
 export const corsHeaders = {
-	'Access-Control-Allow-Origin': '*',
+	'Access-Control-Allow-Origin': process.env.ALLOWED_ORIGINS || 'http://localhost:5173',
 	'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
 	'Access-Control-Allow-Headers': 'Content-Type',
 };
